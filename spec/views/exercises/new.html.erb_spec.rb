6eb92d2e5ec3fc35efd4610021dbc4e5e9701921
ishidamakot <ishida.makot@gmail.com>
2015-02-11 23:47:16 +0000

    All test green

--- spec/views/exercises/new.html.erb_spec.rb
+++ spec/views/exercises/new.html.erb_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "exercises/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'render question and answer' do
+    it {
+      render
+      expect(rendered).to match '問題'
+      expect(rendered).to match '解答'
+    }
+  end
 end

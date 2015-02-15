dc61f24d0769c6a6a5c52e52f12fd9c7b5f1c176
ishidamakot <ishida.makot@gmail.com>
2015-02-15 10:56:06 +0900

    Add exercise spec

--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -17,6 +17,20 @@ RSpec.describe Exercise, type: :model do
     end
 
     describe 'Addition is 6 positive and 4 negative into 1 minus answer' do
+      it {
+        include_minus = exercise.additions.select{ |add|
+          not add.include_minus?
+        }
+        expect(include_minus.size).to eq 6
+      }
+      it {
+        include_minus = exercise.additions.select{|add|add.include_minus?}
+        expect(include_minus.size).to eq 4
+      }
+      it {
+        minus_answer = exercise.additions.select{|add|add.minus_answer?}
+        expect(minus_answer.size).to eq 1
+      }
     end
   end
 end
